6c5aba04e1ec2495075f3b224fa13a12e55d456e
EugeneYamov <evgeny.yamov@yandex.ru>
2022-07-18 23:05:35 +0300

    task 13 is Complete

---
+++ Task_13/Program.cs
@@ -0,0 +1,15 @@
+﻿//Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+ Console.WriteLine("Введите число");
+ string inputNumber = Console.ReadLine(); 
+ int size = inputNumber.Length;
+
+ if (size < 3)
+ {
+     Console.WriteLine("В вашем числе нет третьей цифры");
+ }
+        else
+        {   int NumberIndex = 2;
+            char thirdDigit = inputNumber[NumberIndex];
+            Console.WriteLine($"Третья цифра вашего числа - {thirdDigit}");
+        }
