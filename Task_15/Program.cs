09a3a7278dbdf2f9b4417e2df564ad78f412f9e8
EugeneYamov <evgeny.yamov@yandex.ru>
2022-07-18 23:05:51 +0300

    task 15 is Complete

---
+++ Task_15/Program.cs
@@ -0,0 +1,22 @@
+﻿//Программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите порядковый номер дня недели 1-7");
+int numberDay = Convert.ToInt16(Console.ReadLine());
+
+while (numberDay < 1 || numberDay > 7)
+{    
+    Console.WriteLine("Введите порядковый номер дня недели 1-7");
+    numberDay = Convert.ToInt16(Console.ReadLine());
+}
+
+if (numberDay == 6 || numberDay == 7)
+{
+    Console.WriteLine("Это выходной");
+}
+
+else
+{
+    Console.WriteLine("К сожалению, это рабочий день");
+}
+
+
