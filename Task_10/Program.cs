63267866480f4717aa3c210f3d0d85ea8839abf2
EugeneYamov <evgeny.yamov@yandex.ru>
2022-07-18 23:05:12 +0300

    task 10 is Complete

---
+++ Task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt16(Console.ReadLine());
+
+while (number / 100 < 1 || number / 100  > 9)
+{
+    Console.WriteLine("Введите трёхзначное число");
+    number = Convert.ToInt16(Console.ReadLine());
+}
+
+int secondDigit = number % 100 / 10;
+Console.WriteLine($"Вторая цифра вашего числа - {secondDigit}");
